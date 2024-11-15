7c35149f44f9fd6edd41bd6b09af30f16149cb0a
Arif <business@aryf.de>
2024-11-15 14:48:38 +0100

    Initial Commit

---
+++ SpeicherTEst/SpeicherTEstApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeicherTEstApp.swift
+//  SpeicherTEst
+//
+//  Created by Arif Koc on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct SpeicherTEstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeicherTEstTests/SpeicherTEstTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeicherTEstTests.swift
+//  SpeicherTEstTests
+//
+//  Created by Arif Koc on 15.11.24.
+//
+
+import Testing
+@testable import SpeicherTEst
+
+struct SpeicherTEstTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
